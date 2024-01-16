d430be769a44efb16a191ac9d16ed1c1bf34212e
WonJun Lee <97330728+wonjuneee@users.noreply.github.com>
2024-01-16 19:57:27 +0900

    delete: rollback MyPage

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,12 +1,5 @@
 import { Layout } from '../components/Layout';
-import FavorTeamSche from '../components/FavorTeamSche/FavorTeamSche';
-import { Calendar } from '../components/Calendar/Calendar';
 
 export default function MyPage() {
-  return (
-    <Layout>
-      <Calendar />
-      <FavorTeamSche />
-    </Layout>
-  );
+  return <Layout>MyPage</Layout>;
 }
