5250c4ee4f07699a4156d12ea13dad8aa70fd4a6
hhjeee <yoonhj0315@gmail.com>
2024-01-17 17:26:12 +0900

    feat: favorite team component

--- src/components/FavorTeamSche/FavorTeamSche.tsx
+++ src/components/FavorTeamSche/FavorTeamSche.tsx
@@ -1,230 +1,203 @@
+import { useNavigate } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
+import axios from 'axios';
 import { Grid, Stack, List, ListItemText, Typography, ListItem, Divider } from '@mui/material';
 
 interface Schedule {
   name: string;
-  startDate: Date;
-  endDate: Date;
+  startTime: Date;
+  endTime: Date;
   explanation: string;
 }
-
-interface MyTeamSche {
+interface TeamDetail {
+  teamId: number;
   teamname: string;
-  isFavor: boolean;
-  isHide: boolean;
+  color: number;
   schedule: Schedule[];
+}
+interface TeamInfo {
+  teamId: number;
+  teamname: string;
+  isPublic: number;
+  isAdmin: number;
+  isFavor: number;
+  isHide: number;
 }
 
 export default function FavorTeamSche() {
-  const [favor, setFavor] = useState<MyTeamSche[]>([
-    {
-      teamname: 'test1',
-      isFavor: true,
-      isHide: false,
-      schedule: [
-        {
-          name: 'sche1',
-          startDate: new Date(),
-          endDate: new Date(),
-          explanation: 'sche1',
-        },
-        {
-          name: 'sche2',
-          startDate: new Date(),
-          endDate: new Date(),
-          explanation: 'sche2',
-        },
-      ],
-    },
-    {
-      teamname: 'not Favor test',
-      isFavor: false,
-      isHide: false,
-      schedule: [
-        {
-          name: 'not Favor sche1',
-          startDate: new Date(),
-          endDate: new Date(),
-          explanation: 'not Favor sche1',
-        },
-      ],
-    },
-    {
-      teamname: 'check it out',
-      isFavor: true,
-      isHide: false,
-      schedule: [
-        {
-          name: 'check11111111111111',
-          startDate: new Date('2024-01-11'),
-          endDate: new Date('2024-01-12'),
-          explanation: 'check that it should not be appeared at FavorTeam scheBox',
-        },
-        {
-          name: 'check222222222222',
-          startDate: new Date('2024-01-11'),
-          endDate: new Date('2024-01-14'),
-          explanation:
-            'Check that it should be appeared at FavorTeam scheBox. string이 칸을 넘어갈 경우 공백을 기준으로 줄바꿈이 이루어짐.',
-        },
-        {
-          name: 'check3',
-          startDate: new Date('2024-01-14'),
-          endDate: new Date('2024-01-14'),
-          explanation: 'Check that it should be appeared at FavorTeam scheBox.',
-        },
-        {
-          name: 'check4',
-          startDate: new Date('2024-01-15'),
-          endDate: new Date('2024-01-19'),
-          explanation: 'Check that it should be appeared at FavorTeam scheBox.',
-        },
-        {
-          name: 'check5',
-          startDate: new Date('2024-01-17'),
-          endDate: new Date('2024-01-17'),
-          explanation: 'Check that it should be appeared at FavorTeam scheBox.',
-        },
-        {
-          name: 'check6',
-          startDate: new Date('2024-01-22'),
-          endDate: new Date('2024-01-22'),
-          explanation: 'Check that it should be appeared at FavorTeam scheBox.',
-        },
-        {
-          name: 'check7',
-          startDate: new Date('2024-01-22'),
-          endDate: new Date('2024-01-24'),
-          explanation: 'Check that it should be appeared at FavorTeam scheBox.',
-        },
-      ],
-    },
-  ]);
+  const navigate = useNavigate();
+
+  const [myTeam, setMyTeam] = useState<TeamInfo[]>([]);
+  const [teamDetails, setTeamDetails] = useState<TeamDetail[]>([]);
+
+  const navigateToTeam = (teamId: number) => {
+    navigate(`/team/${teamId}`);
+  };
 
   useEffect(() => {
-    setFavor((curr) => curr);
-  }, []);
-  /*
-  useEffect(() => {
-    setFavor((curr) => [
-      ...curr,
-      {
-        teamname: 'test2',
-        isFavor: true,
-        isHide: false,
-        schedule: [
-          {
-            name: 'test2',
-            startDate: new Date(),
-            endDate: new Date(),
-            explanation: 'test2',
+    // 전체 팀 정보
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/team`, {
+          headers: {
+            'Content-Type': 'application/json',
           },
-        ],
-      },
-    ]);
+          withCredentials: true,
+        });
+
+        if (response.status === 200) {
+          setMyTeam(response.data);
+        }
+      } catch (e) {
+        /* empty */
+      }
+    };
+    fetchData();
   }, []);
-  */
-  const validTeam: MyTeamSche[] = [];
-  for (let i = 0; i < favor.length; i += 1) {
-    if (validTeam.length === 3) break;
-    if (favor[i].isFavor === true && favor[i].isHide === false) {
-      validTeam.push(favor[i]);
+
+  useEffect(() => {
+    // 즐겨찾기 팀 정보
+    const FavorTeamIds = myTeam.filter((team) => team.isFavor === 1).map((team) => team.teamId);
+
+    const fetchAllTeamsInfo = async () => {
+      await Promise.all(FavorTeamIds.map(fetchTeamInfo));
+    };
+
+    if (FavorTeamIds.length > 0) {
+      fetchAllTeamsInfo();
     }
-  }
-  const validSche = validTeam.map<MyTeamSche>((team) => {
+  }, [myTeam]);
+
+  const fetchTeamInfo = async (teamId: number) => {
+    try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/team/${teamId}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      });
+      if (response.status === 200) {
+        setTeamDetails((prevDetails) => {
+          // 이미 로드된 팀 정보를 다시 추가하지 않도록
+          if (prevDetails.some((detail) => detail.teamname === response.data.teamname)) {
+            return prevDetails;
+          }
+          const newDetails = [
+            ...prevDetails,
+            {
+              teamId,
+              teamname: response.data.teamname,
+              color: response.data.color,
+              schedule: response.data.schedules,
+            },
+          ];
+          // 팀이름 기준 정렬
+          newDetails.sort((a, b) => a.teamname.localeCompare(b.teamname));
+          return newDetails;
+        });
+      }
+    } catch (error) {
+      /* empty */
+    }
+  };
+
+  const validSche = teamDetails.map<TeamDetail>((team) => {
     return {
+      teamId: team.teamId,
       teamname: team.teamname,
-      isFavor: team.isFavor,
-      isHide: team.isHide,
+      color: team.color,
       schedule: team.schedule
         .filter((sche) => {
-          return format(sche.endDate, 'y-M-d') >= format(new Date(), 'y-M-d');
+          return format(new Date(sche.endTime), 'y-M-d') >= format(new Date(), 'y-M-d');
         })
-        .sort((a, b) => (format(a.startDate, 'y-M-d') < format(b.startDate, 'y-M-d') ? -1 : 1)),
+        .sort((a, b) => (format(new Date(a.startTime), 'y-M-d') < format(new Date(b.startTime), 'y-M-d') ? -1 : 1)),
     };
   });
 
-  const scheBox = [];
-  for (let i = 0; i < validSche.length; i += 1) {
-    scheBox.push(
-      <Grid>
-        <List
-          sx={{
-            bgcolor: '#cfe8fc',
-            width: '20vw',
-            height: '50vh',
-            borderRadius: '20px',
-            fontSize: '4vh',
-            fontWeight: 'bold',
-            boxShadow: 3,
-            overflow: 'hidden',
-            [`&.MuiList-root:hover`]: {
-              transition: '0.2s ease-in-out',
-              transform: 'scale(1.05)',
-              boxShadow: '4',
-            },
-            [`&.MuiList-root:not(:hover)`]: { transition: '0.2s ease-in-out', transfrom: 'scale(1)' },
-          }}
-        >
-          <li key={validSche[i].teamname}>
-            <ListItem>{validSche[i].teamname}</ListItem>
-
-            <Divider />
-
-            {validSche[i].schedule.map((sche) => (
-              <ListItem
-                sx={{
-                  paddingTop: 0.5,
-                  paddingBottom: 0,
-                  width: '20vw',
-                }}
-              >
-                <ListItemText
-                  primary={
-                    <Typography
-                      component="div"
-                      variant="body2"
-                      color="text.primary"
-                      width={'100%'}
-                      sx={{
-                        fontSize: '2vh',
-                        fontWeight: '800',
-                      }}
-                    >
-                      {sche.name}
-                    </Typography>
-                  }
-                  secondary={
-                    <React.Fragment>
-                      <Typography
-                        sx={{ display: 'inline', fontSize: '1.6vh', fontWeight: 'bold' }}
-                        component="div"
-                        variant="body2"
-                        color="text.primary"
-                      >
-                        {format(sche.startDate, 'y-M-d') === format(sche.endDate, 'y-M-d')
-                          ? `${format(sche.startDate, 'yy년 MM월 dd일 hh:mm')} ~ ${format(sche.endDate, 'hh:mm')}`
-                          : `${format(sche.startDate, 'yy년 MM월 dd일 hh:mm')} ~ ${format(
-                              sche.endDate,
-                              'yy년 MM월 dd일 hh:mm',
-                            )}`}
-                      </Typography>
-                      <Typography sx={{ fontSize: '1.5vh', fontWeight: 'bold' }}>{sche.explanation}</Typography>
-                    </React.Fragment>
-                  }
-                ></ListItemText>
-              </ListItem>
-            ))}
-          </li>
-        </List>
-      </Grid>,
-    );
-  }
-
   return (
     <Stack display={'flex'} justifyContent={'center'} direction={'row'} spacing={10} width={'88vw'}>
-      {scheBox}
+      <Grid container spacing={2}>
+        {validSche.map((team, index) => (
+          <Grid item xs={12} sm={6} md={4} key={index} onClick={() => navigateToTeam(team.teamId)}>
+            <List
+              sx={{
+                bgcolor: '#d3e9f6',
+                width: '20vw',
+                height: '50vh',
+                borderRadius: '20px',
+                fontSize: '4vh',
+                fontWeight: 'bold',
+                boxShadow: 3,
+                overflow: 'hidden',
+                [`&.MuiList-root:hover`]: {
+                  transition: '0.2s ease-in-out',
+                  transform: 'scale(1.05)',
+                  boxShadow: '4',
+                },
+                [`&.MuiList-root:not(:hover)`]: { transition: '0.2s ease-in-out', transfrom: 'scale(1)' },
+              }}
+            >
+              <li key={team.teamname}>
+                <ListItem>{team.teamname}</ListItem>
+                <Divider />
+
+                {team.schedule.length > 0 ? (
+                  team.schedule.map((sche, scheIndex) => (
+                    <ListItem
+                      sx={{
+                        paddingTop: 0.5,
+                        paddingBottom: 0,
+                        width: '20vw',
+                      }}
+                      key={team.teamname + scheIndex}
+                    >
+                      <ListItemText
+                        primary={
+                          <Typography
+                            component="div"
+                            variant="body2"
+                            color="text.primary"
+                            width={'100%'}
+                            sx={{
+                              fontSize: '2vh',
+                              fontWeight: '800',
+                            }}
+                          >
+                            {sche.name}
+                          </Typography>
+                        }
+                        secondary={
+                          <React.Fragment>
+                            <Typography
+                              sx={{ display: 'inline', fontSize: '1.6vh', fontWeight: 'bold' }}
+                              component="div"
+                              variant="body2"
+                              color="text.primary"
+                            >
+                              {format(sche.startTime, 'y-M-d') === format(sche.endTime, 'y-M-d')
+                                ? `${format(sche.startTime, 'yy년 MM월 dd일 hh:mm')} ~ ${format(sche.endTime, 'hh:mm')}`
+                                : `${format(sche.startTime, 'yy년 MM월 dd일 hh:mm')} ~ ${format(
+                                    sche.endTime,
+                                    'yy년 MM월 dd일 hh:mm',
+                                  )}`}
+                            </Typography>
+                            <Typography sx={{ fontSize: '1.5vh', fontWeight: 'bold' }}>{sche.explanation}</Typography>
+                          </React.Fragment>
+                        }
+                      ></ListItemText>
+                    </ListItem>
+                  ))
+                ) : (
+                  <ListItem>
+                    <Typography sx={{ fontSize: '2vh' }}>예정된 일정이 없어요</Typography>
+                  </ListItem>
+                )}
+              </li>
+            </List>
+          </Grid>
+        ))}
+      </Grid>
     </Stack>
   );
 }
