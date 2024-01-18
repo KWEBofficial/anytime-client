67ad277a5bf99ac4f0de7646a59634db5f800b5b
unknown <dyj0304@korea.ac.kr>
2024-01-18 18:16:20 +0900

    fix: eslint error

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import React from 'react';
-import { ModalContextProvider } from './components/Modal/useModal';
 
+import { ModalContextProvider } from './components/Modal/useModal';
 import App from './App';
 import './index.css';
-import Sidebar from './components/Sidebar';
 
 // 리액트로 짠 모든 코드가 App 컴포넌트 안에 들어가게 됩니다.
 // root를 찾아서 App 컴포넌트를 렌더링합니다.
